2301697c8d7a8bdd9455424e08d0d93f8b2d6803
Subash Perumal <subas049@gmail.com>
2020-11-05 11:18:36 +0530

    added wait

--- selenium/src/test/java/com/selenium/ZoomHomePage.java
+++ selenium/src/test/java/com/selenium/ZoomHomePage.java
@@ -1,7 +1,11 @@
 package com.selenium;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ZoomHomePage {
 
@@ -18,6 +22,14 @@ public class ZoomHomePage {
 		// Maximize browser		 
 		driver.manage().window().maximize();
 		
+		// add implicit wait 
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
+		
+			
 		driver.findElement(By.name("email")).sendKeys("arulvma@gmail.com");
 		driver.findElement(By.name("password")).sendKeys("Zum.YouS97");
 		//driver.findElement(By.className("btn btn-primary submit signin user").clic
