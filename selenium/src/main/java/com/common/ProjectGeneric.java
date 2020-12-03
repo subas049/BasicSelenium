6e4d14b21ad4be0c816968decb037d6683555f59
Subash Perumal <subas049@gmail.com>
2020-12-03 11:42:58 +0530

    updated testNG

--- selenium/src/main/java/com/common/ProjectGeneric.java
+++ selenium/src/main/java/com/common/ProjectGeneric.java
@@ -12,18 +12,55 @@ public class ProjectGeneric extends GenericImplementation {
 	public static String passwordLocator = prop.getProperty("passWordTextBox_ID");
 	public static String submitbtnLocator = prop.getProperty("submitButton_Classname");
 	
-	@BeforeSuite
-	public void beforeTest() throws Exception {
+	@BeforeMethod
+	public void beforeMethod() throws Exception {
 		invokeApp();
 		System.out.println("application launched successfully");
 	}
 	
-	@AfterSuite
-	public void afterTest() {
+	@AfterMethod
+	public void afterMethod() {
 		
 		closeBrowser();
 		System.out.println("Browser closed successfully");
 		
 	}
-
+	@BeforeSuite
+	public void beforeSuite() throws Exception {
+		
+		System.out.println("Im from before suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		
+		
+		System.out.println("Im from after suite");
+		
+	}
+	
+	@BeforeClass
+	public void beforeClass() throws Exception {
+		
+		System.out.println("Im from before Class");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+		
+		System.out.println("Im from after Class");
+		
+	}
+	
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		System.out.println("Im from before Test");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		
+		System.out.println("Im from after Test");
+	}
 }
