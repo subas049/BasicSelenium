97e47a93ab3e00236a3dd071c0a51f6bc09f4c36
Subash Perumal <subas049@gmail.com>
2020-12-07 10:54:08 +0530

    TestNG priority go through 12Noc2020

--- selenium/src/test/java/com/tests/TC001_LogintoApplication.java
+++ selenium/src/test/java/com/tests/TC001_LogintoApplication.java
@@ -12,18 +12,33 @@ import com.pages.SignInPage;
 
 public class TC001_LogintoApplication extends ProjectGeneric {
 
-	@Test
-	public void loginToApplication() throws Exception {		
+	@Test(priority = 0)
+	public void vloginToApplication() throws Exception {
 		
+		System.out.println("I am vloginToApplication Method and My priority=0");		
 		new SignInPage().enterUsername().enterPassWord().clickSubmit().verifyHomePage();
 		
 		
 	}
 	
-	@Test
-	public void verifyCRMButton() throws Exception {
-		
+	@Test(priority = 0)
+	public void lverifyCRMButton() throws Exception {
+		System.out.println("I am lverifyCRMButton Method and My priority=0");
 		new SignInPage().enterUsername().enterPassWord().clickSubmit().verifyHomePage().clickCRMBtn().verifyMyHomPage();
 		
 	}
+	
+	@Test(priority = 1)
+	public void verifyMyHomPag() throws Exception {
+		
+		System.out.println("I am verifyMyHomPag Method and My priority=1");
+		new SignInPage().enterUsername().enterPassWord().clickSubmit().verifyHomePage().clickCRMBtn().verifyMyHomPage();
+		
+	}
+	
+	
+	
+	
+	
+	
 }
